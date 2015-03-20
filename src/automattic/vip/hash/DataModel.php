40ceb97e290903828aa4b90ee2506825f46afa96
Tarendai <contact@tomjn.com>
2015-03-20 17:04:44 +0000

    implement marking and retrieving hashes, @todo validate status values and store timestamps

--- src/automattic/vip/hash/DataModel.php
+++ src/automattic/vip/hash/DataModel.php
@@ -2,11 +2,22 @@
 
 namespace automattic\vip\hash;
 
+// @TODO: we should use error objects or exceptions here instead of just returning false
 
 class DataModel {
 
 	public function markHash( $hash, $username, $value ) {
-		return false;
+		$file = $this->getDBDir().$hash.'-'.$username.'.dat';
+		if ( file_exists( $file ) ) {
+			// it already exists! we don't edit/update records, we only add and retrieve them
+			return false;
+		}
+		touch( $file );
+		$result = file_put_contents( $file, $value );
+		if ( !$result ) {
+			return false;
+		}
+		return true;
 	}
 
 	public function hashFile( $file ) {
@@ -19,10 +30,21 @@ class DataModel {
 	}
 
 	public function getHashStatusByUser( $hash, $username ) {
+		$file = $this->getDBDir().$hash.'-'.$username.'.dat';
+		if ( file_exists( $file ) ) {
+			return file_get_contents( $file );
+		}
 		return false;
 	}
 
 	public function getHashStatusAllUsers( $hash ) {
 		return array();
+	}
+
+	/**
+	 * @return string the folder containing hash records with a trailing slash
+	 */
+	protected function getDBDir() {
+		return $_SERVER['HOME'].'/.viphash/';
 	}
 } 
