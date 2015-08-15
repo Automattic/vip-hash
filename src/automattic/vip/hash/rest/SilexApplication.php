e016d140d9c57d5f81bdfe30ad5f247ecac827dd
Tom J Nowell <contact@tomjn.com>
2015-08-15 13:26:37 +0100

    implement 2 endpoints

--- src/automattic/vip/hash/rest/SilexApplication.php
+++ src/automattic/vip/hash/rest/SilexApplication.php
@@ -2,16 +2,42 @@
 
 namespace automattic\vip\hash\rest;
 
+use automattic\vip\hash\DataModel;
+use automattic\vip\hash\HashRecord;
+
 class SilexApplication {
 	public function run() {
 		$app = new Silex\Application();
 
 		$this->register_endpoints( $app );
 
+		$app->view( function ( array $controllerResult ) use ( $app ) {
+			return $app->json( $controllerResult );
+		});
+
 		$app->run();
 	}
 
 	public function register_endpoints( Silex\Application $app ) {
-		//
+		/**
+		 * remote/hash <- send
+		 * remote/hash/<hash> <- read
+		 * remote/hash/seen/since/<time> <- read
+		 */
+		$app->get( '/hash/seen/since/{timestamp}', function( $timestamp ) use( $app ) {
+			$model = new DataModel();
+			return $model->getHashesAfter( $timestamp );
+		});
+
+		$app->get( '/hash/{hash}', function( $hash ) use( $app ) {
+			$data = new DataModel();
+			try {
+				return $model->getHashStatusAllUsers( $hash );
+			} catch( \Exception $e ) {
+				return array( 'error' => $e->getMessage() );
+			}
+		});
+
+
 	}
 }
