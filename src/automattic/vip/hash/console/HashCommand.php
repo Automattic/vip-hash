8e38b7309152cb4400fa4746f25a196a6599f852
Tom J Nowell <contact@tomjn.com>
2016-08-04 22:37:12 +0100

    add some rudimentary PHPDocs to the HashCommand class

--- src/automattic/vip/hash/console/HashCommand.php
+++ src/automattic/vip/hash/console/HashCommand.php
@@ -2,14 +2,21 @@
 
 namespace automattic\vip\hash\console;
 
-use automattic\vip\hash\DataModel;
 use automattic\vip\hash\Pdo_Data_Model;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Class HashCommand
+ * @package automattic\vip\hash\console
+ */
 class HashCommand extends Command {
+
+	/**
+	 *
+	 */
 	protected function configure() {
 		$this->setName( 'hash' )
 			->setDescription( 'take a file and generate a hash representing said file' )
@@ -20,6 +27,13 @@ class HashCommand extends Command {
 			);
 	}
 
+	/**
+	 * @param InputInterface  $input
+	 * @param OutputInterface $output
+	 *
+	 * @return void
+	 * @throws \Exception
+	 */
 	protected function execute( InputInterface $input, OutputInterface $output ) {
 		$file = $input->getArgument( 'file' );
 		if ( empty( $file ) ) {
