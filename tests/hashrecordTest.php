3f7cfed0ed1929e52277e863b49038a32ea365dd
Tom J Nowell <contact@tomjn.com>
2016-08-12 03:04:35 +0100

    extend the hash record test so that the hashrecord class has 100% coverage

--- tests/hashrecordTest.php
+++ tests/hashrecordTest.php
@@ -8,12 +8,32 @@ class HashRecordTest extends TestCase {
 	public function testGetterSetters() {
 		$note = 'Lorem Ipsum';
 		$human_note = 'Lorem Ipsum dolors';
+		$username = 'john smith';
+		$status = 'great';
+		$hash = '1234';
+		$date = 1470957266;
+
 		$record = new HashRecord();
 
 		$record->setNote( $note );
 		$record->setHumanNote( $human_note );
+		$record->setUsername( $username );
+		$record->setStatus( $status );
+		$record->setHash( $hash );
+		$record->setDate( $date );
+		$data = $record->getData();
 
 		$this->assertEquals( $note, $record->getNote() );
 		$this->assertEquals( $human_note, $record->getHumanNote() );
+		$this->assertEquals( $username, $record->getUsername() );
+		$this->assertEquals( $status, $record->getStatus() );
+		$this->assertEquals( $hash, $record->getHash() );
+		$this->assertEquals( $date, $record->getDate() );
+
+		$this->assertEquals( $data['hash'], $hash );
+
+		$newdata = [1,2,3,4];
+		$record->setData( $newdata );
+		$this->assertEquals( $newdata, $record->getData() );
 	}
 }
