68ea080e0868cc35d91d17e48d60bf8572ebc0cc
Tom J Nowell <contact@tomjn.com>
2016-08-21 23:51:27 +0100

    colourize the output of the sync command and change the endpoints used to match a WP API route

--- src/automattic/vip/hash/console/SyncCommand.php
+++ src/automattic/vip/hash/console/SyncCommand.php
@@ -29,6 +29,10 @@ class SyncCommand extends Command {
 				'remote',
 				InputArgument::REQUIRED,
 				'the name of a remote source'
+			)->addArgument(
+				'option',
+				InputArgument::OPTIONAL,
+				'an additional option'
 			);
 	}
 
@@ -46,24 +50,40 @@ class SyncCommand extends Command {
 			throw new \Exception( 'There was an issue trying to get the remotes information, does this remote name exist?' );
 		}
 
-		$output->writeln( 'Synchronising hashes with ' . $remote->getName() . ' - ' . $remote->getUri() );
+		$output->writeln( '<comment>Synchronising hashes with ' . $remote->getName() . ' - ' . $remote->getUri().'</comment>' );
 
-		$output->writeln( 'Fetching new remote hashes' );
+		$output->writeln( '<comment>Fetching new remote hashes</comment>' );
 		$hashes = $this->fetchHashes( $remote );
+		if ( $hashes === false ) {
+			$output->writeln( "<error>Fetching hashes failed</error>" );
+			return;
+		}
+		if ( count( $hashes ) == 0 ) {
+			$output->writeln( '<info>No new hashes available</info>' );
+		}
 
-		$output->writeln( 'Sending new local hashes' );
-		$this->sendHashes( $remote, $output, $data );
 
-		$this->saveHashes( $hashes, $output, $data );
+		$output->writeln( '<comment>Sending new local hashes</comment>' );
+		$success = $this->sendHashes( $remote, $output, $data );
+		if ( false === $success ) {
+			$output->writeln( '<error>Sending new local hashes failed</error>' );
 
-		$output->writeln( 'Updating remote record' );
+			return;
+		}
+
+		if ( !empty( $hashes ) ) {
+			$output->writeln( "<comment>Saving the new hashes</comment>" );
+			$this->saveHashes( $hashes, $output, $data );
+		}
+
+		$output->writeln( '<comment>Updating remote record</comment>' );
 		$latest_hash = $data->getNewestSeenHash();
 		$remote->setLatestSeen( $latest_hash['seen'] );
 		$remote->setLastSent( time() );
 		$saved = $remote->save( $data );
-		$message = 'Failed to save remote record';
+		$message = '<error>Failed to save remote record</error>';
 		if ( $saved ) {
-			$message = 'Saved remote record';
+			$message = '<info>Saved remote record</info>';
 		}
 		$output->writeln( $message );
 
@@ -85,12 +105,23 @@ class SyncCommand extends Command {
 		 * Finish by retrieving the data from the remote end that we don't have
 		 */
 
-		/** @noinspection PhpVoidFunctionResultUsedInspection */
-		$response = $client->get( $remote->getUri() . 'hash/seen/since/' . $i_saw );
+		/**
+		 * @var: $response \GuzzleHttp\Message\ResponseInterface
+		 */
+		$response = $client->get( $remote->getUri() . 'hashes?since=' . $i_saw );
+		if ( $response->getStatusCode() != 200 ) {
+			echo "Problem response code? ".$response->getStatusCode()."\n";
+			return false;
+		}
 		$new_items = $response->json();
 		return $new_items;
 	}
 
+	/**
+	 * @param array           $hashes
+	 * @param OutputInterface $output
+	 * @param DataModel       $data
+	 */
 	protected function saveHashes( array $hashes, OutputInterface $output, DataModel $data ) {
 		if ( empty( $hashes ) ) {
 			$output->writeln( 'No new hashes recieved' );
@@ -117,15 +148,18 @@ class SyncCommand extends Command {
 	 * @param OutputInterface $output
 	 *
 	 * @param DataModel       $data
+	 *
+	 * @return bool
 	 */
 	protected function sendHashes( Remote $remote, OutputInterface $output, DataModel $data ) {
 		$i_sent = $remote->getLastSent();
 
 		$send_data = $data->getHashesSeenAfter( $i_sent );
 		if ( empty( $send_data ) ) {
-			$output->writeln( 'No hashes to send' );
+			$output->writeln( '<info>No hashes to send</info>' );
+			return true;
 		}
-		$output->writeln( 'Hashes to send: '. count( $send_data ) );
+		$output->writeln( '<info>Hashes to send: '. count( $send_data )."</info>" );
 
 		// don't send a request with thousands of hashes all at once,
 		// some servers have request size limits
@@ -136,33 +170,50 @@ class SyncCommand extends Command {
 		$counter = 0;
 		foreach ( $chunks as $chunk ) {
 			$counter ++;
-			$output->writeln( 'Sending chunk : '. $counter .' of '. count( $chunks ) );
+			$output->writeln( '<info>Sending chunk : '. $counter .' of '. count( $chunks )."</info>" );
 			$sent = $this->sendHashChunk( $chunk, $remote, $output );
 			// if something went wrong, don't continue sending chunks
 			if ( ! $sent ) {
-				break;
+				$output->writeln( "<error>Chunk ". $counter .' of '. count( $chunks ) ." failed to send</error>" );
+				return false;
 			}
 		}
+		return true;
 	}
 
+	/**
+	 * @param array           $data
+	 * @param Remote          $remote
+	 * @param OutputInterface $output
+	 *
+	 * @return bool
+	 */
 	protected function sendHashChunk( array $data, Remote $remote, OutputInterface $output ) {
 		$client = new Client();
 		$send_data = json_encode( $data );
 		try {
-			/** @noinspection PhpVoidFunctionResultUsedInspection */
-			$response = $client->post( $remote->getUri() . 'hash', [
+			/**
+			 * @var: $response \GuzzleHttp\Message\ResponseInterface
+			 */
+			$response = $client->post( $remote->getUri() . 'hashes', [
 				'body' => [
 					'data' => $send_data,
 				],
 			] );
 			// @TODO: do something with the response
 			//$json = $response->json();
+			if ( $response->getStatusCode() != 200 ) {
+				echo "Problem response code? ".$response->getStatusCode()."--\n";
+				echo $response->getBody()->getContents()."|";
+				return false;
+			}
 			$remote->setLastSent( time() );
 		} catch ( ServerException $e) {
 			$output->writeln( 'Guzzle ServerException: ' . $e->getResponse() );
+			$output->writeln( $e->getMessage() );
 			return false;
 		} catch ( ParseException $e ) {
-			$output->writeln( 'Guzzle JSON issue: ' . $e->getResponse(). ' -- '. $response->getBody() );
+			$output->writeln( 'Guzzle JSON issue: ' . $e->getResponse() );
 			return false;
 		}
 		return true;
