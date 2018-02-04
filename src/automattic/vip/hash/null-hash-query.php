06275e2b59a6824dc92f4296814f597f2b8dd178
Tom J Nowell <contact@tomjn.com>
2018-02-04 20:37:15 +0000

    inherit phpdocs in the null hash query object

--- src/automattic/vip/hash/null-hash-query.php
+++ src/automattic/vip/hash/null-hash-query.php
@@ -4,15 +4,30 @@ namespace automattic\vip\hash;
 
 
 class NullHashQuery implements HashQuery {
+	/**
+	 * @inherit
+	 */
 	public function fetch( array $arguments ) : bool {
 		return false;
 	}
+
+	/**
+	 * @inherit
+	 */
 	public function totalPages() : int {
 		return 0;
 	}
+
+	/**
+	 * @inherit
+	 */
 	public function hashes() : array {
 		return [];
 	}
+	
+	/**
+	 * @inherit
+	 */
 	public function hashCount() : int {
 		return 0;
 	}
