62b12a18647fd9816f061446f5690170773a0390
Tom J Nowell <contact@tomjn.com>
2018-02-04 20:36:54 +0000

    add phpdocs to hash query interface

--- src/automattic/vip/hash/hash-query.php
+++ src/automattic/vip/hash/hash-query.php
@@ -2,11 +2,35 @@
 
 namespace automattic\vip\hash;
 
-
+/**
+ * An interface for fetching hashes from a data store
+ */
 interface HashQuery {
-	//
+	/**
+	 * Fetches hashes given arguments
+	 * 
+	 * @param  array  $arguments A dictionary array containing parameters for what hashes to apc_fetch
+	 * @return  bool              True if the hashes were fetched false for anything else, may throw exceptions
+	 */
 	public function fetch( array $arguments ) : bool;
+
+	/**
+	 * The number of pages if pagination is enabled
+	 * @return int 0 if an error occurred, 1 if pagination is off, more if pagination is enabled
+	 */
 	public function totalPages() : int;
+
+	/**
+	 * An array of the found hashes
+	 * 
+	 * @return array An array of the hashes found when `fetch` was called, will be empty if that hasn't happened yet
+	 */
 	public function hashes() : array;
+
+	/**
+	 * How many hashes were fetched
+	 * 
+	 * @return int Number of hashes fetched
+	 */
 	public function hashCount() : int;
 }
